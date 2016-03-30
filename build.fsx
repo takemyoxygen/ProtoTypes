ec9236ebf843c94fffa7a2e3600849f23c2285b2
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-03-30 22:09:12 +0200

    Simplify build.fsx

--- build.fsx
+++ build.fsx
@@ -1,42 +1,21 @@
-// include Fake libs
 #r "./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
 
-// Directories
-let buildDir  = "./build/"
-let deployDir = "./deploy/"
+let outputDirectory  = "./build/"
+let projects  = !! "/**/*.fsproj"
+let version = "0.1" 
 
-
-// Filesets
-let appReferences  =
-    !! "/**/*.csproj"
-      ++ "/**/*.fsproj"
-
-// version info
-let version = "0.1"  // or retrieve from CI server
-
-// Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
+    CleanDir outputDirectory
 )
 
 Target "Build" (fun _ ->
-    // compile all projects below src/app/
-    MSBuildDebug buildDir "Build" appReferences
-        |> Log "AppBuild-Output: "
+    MSBuildDebug outputDirectory "Build" projects
+    |> Log "AppBuild-Output: "
 )
 
-Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
-        -- "*.zip"
-        |> Zip buildDir (deployDir + "ApplicationName." + version + ".zip")
-)
-
-// Build order
 "Clean"
   ==> "Build"
-  ==> "Deploy"
 
-// start build
 RunTargetOrDefault "Build"
