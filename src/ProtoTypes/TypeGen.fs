57b01e8b3be5e6d2ff0c5cd67d8e6f8ce7c63264
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-03-31 23:32:13 +0200

    Generate property getters

--- src/ProtoTypes/TypeGen.fs
+++ src/ProtoTypes/TypeGen.fs
@@ -1,5 +1,7 @@
 namespace ProtoTypes
 
+open System
+
 open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Core.CompilerServices
 
@@ -9,20 +11,25 @@ open Froto.Parser.Model
 [<RequireQualifiedAccess>]
 module internal TypeGen =
 
+    let private toCamelCase (s: string) =
+        s.Split('_')
+        |> Seq.map (fun s -> Char.ToUpper(s.[0]).ToString() + (if s.Length > 1 then s.Substring(1) else String.Empty) )
+        |> String.concat String.Empty
+
     let private propertyForField (field: ProtoField) =
-        let fieldType = 
+        let fieldType, value = 
             match field.Type with
-            | "int32" -> typeof<int>
-            | "string" -> typeof<string>
+            | "int32" -> typeof<int>, box 0
+            | "string" -> typeof<string>, box "string"
             | _ -> notsupportedf "Field type %s" field.Type
             
-        let propertyType = 
+        let propertyType, propertyValue = 
             match field.Rule with
-            | Required -> fieldType
-            | Optional -> typedefof<Option<_>>.MakeGenericType(fieldType)
-            | Repeated -> typedefof<list<_>>.MakeGenericType(fieldType)
+            | Required -> fieldType, value
+            | Optional -> typedefof<Option<_>>.MakeGenericType(fieldType), box <| Some value
+            | Repeated -> typedefof<list<_>>.MakeGenericType(fieldType), box [value]
             
-        ProvidedProperty(field.Name, propertyType)
+        ProvidedProperty(toCamelCase field.Name, propertyType, GetterCode = (fun _ -> <@@ value @@>))
 
     let typeForMessage (message: ProtoMessage) = 
         let messageType = new ProvidedTypeDefinition(message.Name, Some typeof<obj>)
