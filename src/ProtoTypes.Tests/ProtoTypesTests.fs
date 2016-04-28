a2a218ba0c7e2e86ea045a528b3697bde4ce4f8c
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-04-28 08:19:24 +0200

    Add simple test to check that serialization doesn't fail

--- src/ProtoTypes.Tests/ProtoTypesTests.fs
+++ src/ProtoTypes.Tests/ProtoTypesTests.fs
@@ -6,6 +6,8 @@ open System
 open NUnit.Framework
 open FsUnit
 
+open Froto.Core
+
 type Proto = ProtoTypes.ProtocolBuffersTypeProvider<"proto/person.proto">
 type Sample = Proto.ProtoTypes.Sample
 
@@ -16,3 +18,14 @@ let ``Person test``() =
     p.Name |> should be (equal "Name")
     p.PersonGender |> should be (equal Sample.Person.Gender.Female)
     p.PersonAddress.Value.Address1 |> should be (equal "Street")
+    
+[<Test>]
+let ``Serialization test``() =
+    let address = Sample.Person.Address("Street", 12, [1; 2; 3], [Sample.Person.IntContainer(5); Sample.Person.IntContainer(7)])
+    let person = Sample.Person("Name", 1, false, 82.3, Sample.Person.Gender.Female, Some "Email", Some address)
+    
+    let buffer = ZeroCopyBuffer(1000)
+    person.Serialize(buffer)
+    
+    buffer.Position |> should be (greaterThan 0)
+    
